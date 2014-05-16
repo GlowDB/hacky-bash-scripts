41b7d705fa300aa9bed97d05da755fcde823edf2
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-05-16 11:13:14 +0000

    updated user.sh

--- user.sh
+++ user.sh
@@ -1,11 +1,7 @@
-#User setup for new server
 USER=$1
 PASSWORD=$2
 
 sudo useradd -m -d /home/$USER -s /bin/bash -U $USER
-
-ID=$(id -u $USER)
-
 sudo groupadd admin
 sudo usermod -g admin $USER
-sudo echo $ID':'$PASSWORD | chpasswd
+echo $USER':'$PASSWORD | chpasswd
